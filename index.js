c50fee5d4904314ee1cabbdc2b1c8d89e75c963f
RomanTsvetik <zvetik85@gmail.com>
2021-11-12 14:10:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const sum = (a, b) => {
+    const c = a + b;
+    return c;
+};
+function dif(a, b) {
+    return a - b;
+}
+const mult = (a, b) => {
+    const c = a * b;
+    return c;
+};
+sum(2, 3);
+dif(5, 4);
+mult(6, 4);
+
